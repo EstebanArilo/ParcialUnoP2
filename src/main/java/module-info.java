07e8acf8d75eaf31a8c4747f2dd08cfa478bb125
esteban <esteban.aristizaball.@uqvirtual.edu.co>
2026-09-23 17:03:38 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module uqvirutal.edu.co.parcialunop2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens uqvirutal.edu.co.parcialunop2 to javafx.fxml;
+    exports uqvirutal.edu.co.parcialunop2;
+}
---
+++ src/main/java/uqvirutal/edu/co/parcialunop2/Launcher.java
@@ -0,0 +1,9 @@
+package uqvirutal.edu.co.parcialunop2;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
