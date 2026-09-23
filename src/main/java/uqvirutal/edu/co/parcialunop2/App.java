5951207ca1cc078bd066ebfe419f5620f6fd1344
esteban <esteban.aristizaball.@uqvirtual.edu.co>
2026-09-23 17:46:58 -0500

    creacion de los paquete model y controller

--- src/main/java/uqvirutal/edu/co/parcialunop2/App.java
+++ src/main/java/uqvirutal/edu/co/parcialunop2/App.java
@@ -2,7 +2,7 @@ package uqvirutal.edu.co.parcialunop2;
 
 import javafx.application.Application;
 
-public class Launcher {
+public class App {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
     }
